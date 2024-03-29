3a9acbb4b7ba351dbdb376c04b3f66870b102101
faqihcyber27 <34843362+faqihcyber27@users.noreply.github.com>
2024-03-29 12:54:45 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -145,6 +145,17 @@ export default function Home() {
               /></a>
             </div>
           </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1">
+              <a href="https://www.kalihcoffeetegal.shop"><Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                
+              /></a>
+            </div>
+          </div>
         </section>
       </main>
     </div>
